c0f93fd18bd942da30b41aa099c5cef9047e9fe1
LYarmoliuk <ludka.yarmolyuk@gmail.com>
2026-05-04 19:14:19 +0300

    feat(tournaments): add API service and routing infrastructure (DEV-162)

--- client/src/views/CreateTournamentPage.vue
+++ client/src/views/CreateTournamentPage.vue
@@ -7,17 +7,39 @@
     </section>
 
     <section class="create-tournament-page__content">
-      <CreateTournamentForm />
+      <CreateTournamentForm @created="handleTournamentCreated" />
     </section>
 
     <SiteFooter />
+
+    <AddPlayersModal
+      v-if="createdTournament"
+      :tournament-id="createdTournament.id"
+      @close="handleModalClose"
+    />
   </main>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 import AppHeader from '../components/AppHeader.vue';
 import SiteFooter from '../components/SiteFooter.vue';
 import CreateTournamentForm from '../components/forms/CreateTournamentForm.vue';
+import AddPlayersModal from '../components/modals/AddPlayersModal.vue';
+import type { ITournamentResponse } from '../types/Tournament';
+
+const router = useRouter();
+const createdTournament = ref<ITournamentResponse | null>(null);
+
+const handleTournamentCreated = (tournament: ITournamentResponse) => {
+  createdTournament.value = tournament;
+};
+
+const handleModalClose = () => {
+  createdTournament.value = null;
+  router.push('/my-tournaments');
+};
 </script>
 
 <style scoped>
@@ -25,37 +47,6 @@ import CreateTournamentForm from '../components/forms/CreateTournamentForm.vue';
   min-height: 100vh;
   background: #151d22;
   color: #fffcf2;
-}
-
-.create-tournament-page__hero {
-  min-height: 270px;
-  padding: 126px 16px 0;
-  background:
-    linear-gradient(rgba(0, 10, 32, 0.2), rgba(0, 10, 32, 0.75)),
-    url('../assets/hero-bg.png') center / cover no-repeat;
-  text-align: center;
-}
-
-.create-tournament-page__hero h1 {
-  margin: 0;
-  font-size: 48px;
-  font-weight: 800;
-  line-height: 1.15;
-}
-
-.create-tournament-page__content {
-  margin-top: 30px;
-  padding-bottom: 152px;
-}
-
-@media (max-width: 900px) {
-  .create-tournament-page__hero {
-    padding-top: 110px;
-  }
-
-  .create-tournament-page__hero h1 {
-    font-size: 36px;
-  }
 }
 
 .create-tournament-page__hero {
@@ -77,5 +68,11 @@ import CreateTournamentForm from '../components/forms/CreateTournamentForm.vue';
 .create-tournament-page__content {
   margin-top: -30px;
   padding-bottom: 152px;
+}
+
+@media (max-width: 900px) {
+  .create-tournament-page__hero h1 {
+    font-size: 36px;
+  }
 }
 </style>
